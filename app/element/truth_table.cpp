418e3fd420b5ccba187945e42c467a4ba6d21a2d
Matheus <matheus.resteves@gmail.com>
2024-03-06 17:18:52 -0300

    add updatePortsProperties implementation

--- app/element/truth_table.cpp
+++ app/element/truth_table.cpp
@@ -4,6 +4,7 @@
 #include "truth_table.h"
 
 #include "globalproperties.h"
+#include "qneport.h"
 
 namespace
 {
@@ -16,58 +17,57 @@ TruthTable::TruthTable(QGraphicsItem *parent)
     if (GlobalProperties::skipInit) {
         return;
     }
+
     m_defaultSkins << m_pixmapPath;
     m_alternativeSkins = m_defaultSkins;
     setPixmap(0);
     setHasTruthTable(true);
     setCanChangeSkin(true);
     setHasLabel(true);
-}
 
-void TruthTable::generatePixmap()
-{
-    // make pixmap
-    const QSize size = portsBoundingRect().united(QRectF(0, 0, 64, 64)).size().toSize();
-    QPixmap tempPixmap(size);
-    tempPixmap.fill(Qt::transparent);
-
-    QPainter tmpPainter(&tempPixmap);
-
-    tmpPainter.setBrush(QColor(126, 126, 126));
-    tmpPainter.setPen(QPen(QBrush(QColor(78, 78, 78)), 0.5, Qt::SolidLine));
-
-    // draw package
-    QPoint topLeft = tempPixmap.rect().topLeft();
-    topLeft.setX(topLeft.x() + 7);
-    QSize finalSize = tempPixmap.rect().size();
-    finalSize.setWidth(finalSize.width() - 14);
-    QRectF finalRect = QRectF(topLeft, finalSize);
-    tmpPainter.drawRoundedRect(finalRect, 3, 3);
-
-    QPixmap panda(":/basic/truthtable.svg");
-    QPointF pandaOrigin = finalRect.center();
-    pandaOrigin.setX(pandaOrigin.x() - panda.width() / 2);
-    pandaOrigin.setY(pandaOrigin.y() - panda.height() / 2);
-    tmpPainter.drawPixmap(pandaOrigin, panda);
-
-    // draw shadow
-    tmpPainter.setBrush(QColor(78, 78, 78));
-    tmpPainter.setPen(QPen(QBrush(QColor(78, 78, 78)), 0.5, Qt::SolidLine));
-
-    QRectF shadowRect(finalRect.bottomLeft(), finalRect.bottomRight());
-    shadowRect.adjust(0, -3, 0, 0);
-    tmpPainter.drawRoundedRect(shadowRect, 3, 3);
-
-    // draw semicircle
-    QRectF topCenter = QRectF(finalRect.topLeft() + QPointF(18, -12), QSize(24, 24));
-    tmpPainter.drawChord(topCenter, 0, -180 * 16);
-
-    m_pixmap = std::make_unique<QPixmap>(tempPixmap);
+    TruthTable::updatePortsProperties();
 }
 
 void TruthTable::updatePortsProperties()
 {
+    int index = 1;
+    const int step = GlobalProperties::gridSize / 2;
 
+    if (!m_inputPorts.isEmpty()) {
+        int y = 32 - (m_inputPorts.size() * step) + step;
+
+        for (auto *port : qAsConst(m_inputPorts)) {
+
+            if (!isRotatable()) {
+                port->setRotation(0);
+            }
+
+            port->setPos(0, y);
+
+            y += step * 2;
+
+            port->setName("Input (" + QString::number(index)  + ")");
+            index++;
+        }
+    }
+
+    index = 1;
+
+    if (!m_outputPorts.isEmpty()) {
+        int y = 32 - (m_outputPorts.size() * step) + step;
+
+        for (auto *port : qAsConst(m_outputPorts)) {
+
+            if (!isRotatable()) {
+                port->setRotation(0);
+            }
+
+            port->setPos(64, y);
+
+            y += step * 2;
+
+            port->setName("Output (" + QString::number(index)  + ")");
+            index++;
+        }
+    }
 }
-
-
